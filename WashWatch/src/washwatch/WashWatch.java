810fe1af17bde0c2cfda1bbb7a36c29f71b33d3c
roanm <roanm@DESKTOP-2H3MM31>
2015-11-10 01:44:27 -0500

    Started program flow, esp. timing. HAS ERRORS(This is a Roan has to wake  up at 8am commit)

--- WashWatch/src/washwatch/WashWatch.java
+++ WashWatch/src/washwatch/WashWatch.java
@@ -5,20 +5,40 @@
  */
 package washwatch;
 
+
+
 /**
  *
  * @author roanm
  */
 public class WashWatch {
 
+    float initTime;
+    float getInitTime(){ return initTime; }
+    void setInitTime(){ initTime = System.nanoTime().toSeconds(); }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         //Init code
+        setInitTime();
         
         //Main timing loop: simple 1 min loop implementatation
         
     }
     
+    void Initialize()
+    {
+        
+    }
+    
+    float runningTime()
+    {
+        return System.nanoTime().toSeconds() - init time ;
+    }
+    
+    float toSeconds(long nanoSeconds)
+    {
+        return (float)nanoSeconds/1000000000L;
+    }
 }
